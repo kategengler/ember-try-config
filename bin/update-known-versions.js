5c3b48d15dcf8184878ea0f782a2585fca3ec5d8
offirgolan <offirgolan@gmail.com>
2016-09-09 11:51:45 -0700

    Fix logging in preversion script

--- bin/update-known-versions.js
+++ bin/update-known-versions.js
@@ -6,18 +6,17 @@ var fetchVersions = require('../lib/fetch-repo-versions-from-github');
 
 function writeVersionsToFile(file, versions) {
   var uniqVersions = array.uniq(versions);
-  console.log('versions found: ', uniqVersions.length);
+  console.log('Writing ' + uniqVersions.length + ' versions to ' + file);
 
   fs.writeFileSync(file, JSON.stringify(uniqVersions, null, 2), { encoding: 'utf-8'});
 }
 
-console.log('Updating list of ember versions');
+console.log('Updating version lists...');
 fetchVersions('ember', { logErrors: true })
   .then(function(versions) {
     writeVersionsToFile('lib/known-ember-versions.json', versions);
   });
 
-console.log('Updating list of ember-data versions');
 fetchVersions('ember-data', { logErrors: true })
   .then(function(versions) {
     writeVersionsToFile('lib/known-ember-data-versions.json', versions);
