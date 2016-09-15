d7bfb31b61be85949c3365b45189213c2da0e614
offirgolan <offirgolan@gmail.com>
2016-09-15 15:36:13 -0700

    Merge known dependencies with each scenario via ember-new-output

---
+++ bin/test-config.js
@@ -0,0 +1,8 @@
+var util = require('util');
+var generateConfig = require('../lib/auto-scenario-config-for-ember');
+
+generateConfig({ versionCompatibility: { ember: '>1.11.0 <=2.5.0', withEmberData: true }}).then(function(scenarios) {
+  console.log(util.inspect(scenarios, false, null));
+}, function() {
+  console.log('error', arguments);
+});
--- bin/update-known-versions.js
+++ bin/update-known-versions.js
@@ -2,22 +2,57 @@
 
 var fs = require('fs');
 var array = require('lodash/array');
+var semver = require('semver');
+var RSVP = require('rsvp');
 var fetchVersions = require('../lib/fetch-repo-versions-from-github');
+var fetchFile = require('../lib/fetch-file-from-github');
+
+function writeToFile(file, content) {
+  console.log('Writing to file', file);
+  fs.writeFileSync(file, JSON.stringify(content, null, 2), { encoding: 'utf-8'});
+}
 
 function writeVersionsToFile(file, versions) {
   var uniqVersions = array.uniq(versions);
-  console.log('Writing ' + uniqVersions.length + ' versions to ' + file);
-
-  fs.writeFileSync(file, JSON.stringify(uniqVersions, null, 2), { encoding: 'utf-8'});
+  console.log('Writing ' + uniqVersions.length + ' versions');
+  writeToFile(file, uniqVersions);
 }
 
 console.log('Updating version lists...');
 fetchVersions('ember', { logErrors: true })
   .then(function(versions) {
-    writeVersionsToFile('lib/known-ember-versions.json', versions);
+    writeVersionsToFile('data/versions/ember.json', versions);
   });
 
 fetchVersions('ember-data', { logErrors: true })
   .then(function(versions) {
-    writeVersionsToFile('lib/known-ember-data-versions.json', versions);
+    writeVersionsToFile('data/versions/ember-data.json', versions);
+  });
+
+fetchVersions('ember-new-output', { logErrors: true, owner: 'ember-cli' })
+  .then(function(versions) {
+    var uniqVersions = array.uniq(versions);
+    var files = [];
+
+    console.log('Found ' + uniqVersions.length + ' possible dependencies');
+
+    writeVersionsToFile('data/versions/ember-new-output.json', versions);
+
+    uniqVersions.forEach(function(version) {
+      version = semver.clean(version);
+      if (semver.gte(version, '1.13.0')) {
+        var filePrefix = 'data/dependencies/' + version;
+
+        files.push(
+          fetchFile('ember-new-output', version, 'package.json').then(function(json) {
+            writeToFile(filePrefix + '-package.json', json);
+          }),
+          fetchFile('ember-new-output', version, 'bower.json').then(function(json) {
+            writeToFile(filePrefix + '-bower.json', json);
+          })
+        );
+      }
+    });
+
+    return RSVP.allSettled(files);
   });
