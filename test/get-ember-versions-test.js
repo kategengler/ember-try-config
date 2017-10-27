74156bc74ba23f9a1305204ee4c27a7885ff3a6f
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-10-27 13:35:15 +0200

    Sort generated configs according to semver rules

--- test/get-ember-versions-test.js
+++ test/get-ember-versions-test.js
@@ -12,4 +12,14 @@ describe('lib/get-ember-versions', function() {
       expect(versions.length).to.equal(knownVersions.length + 3);
     });
   });
+
+  it('sort versions by semver', function() {
+    return getEmberVersions(['2.13.0', '2.10.0', '2.9.0', '2.8.0', '2.7.0']).then(function(versions) {
+      expect(versions.indexOf('2.13.0')).to.be.below(versions.indexOf('2.10.0'));
+      expect(versions.indexOf('2.10.0')).to.be.below(versions.indexOf('2.9.0'));
+      expect(versions.indexOf('2.9.0')).to.be.below(versions.indexOf('2.8.0'));
+      expect(versions.indexOf('2.8.0')).to.be.below(versions.indexOf('2.7.0'));
+    });
+  });
+
 });
---
+++ test/sort-versions-test.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var expect = require('chai').expect;
+var sortVersions = require('../lib/sort-versions');
+
+describe('lib/sort-versions', function() {
+
+  it('sorts by semver with the most recent version on top', function() {
+    var input = [
+      '2.0.0',
+      '2.13.0',
+      '1.13.4',
+      '2.0.1'
+    ];
+
+    var expected = [
+      '2.13.0',
+      '2.0.1',
+      '2.0.0',
+      '1.13.4'
+    ];
+
+    expect(sortVersions(input)).to.deep.equal(expected);
+  });
+
+  it('can handle "v" prefix', function() {
+    var input = [
+      '2.0.0',
+      '2.13.0',
+      '1.13.4',
+      'v2.0.1'
+    ];
+
+    var expected = [
+      '2.13.0',
+      'v2.0.1',
+      '2.0.0',
+      '1.13.4'
+    ];
+
+    expect(sortVersions(input)).to.deep.equal(expected);
+  });
+
+
+  it('sorts non-semver versions alphabetically at the bottom', function() {
+    var input = [
+      '2.0.0',
+      '2.13.0',
+      'foo',
+      'bar'
+    ];
+
+    var expected = [
+      '2.13.0',
+      '2.0.0',
+      'bar',
+      'foo'
+    ];
+
+    expect(sortVersions(input)).to.deep.equal(expected);
+  });
+
+});
