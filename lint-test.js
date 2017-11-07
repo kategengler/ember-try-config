550aea4aa62042c5993dec06515d029724020df3
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-07 08:36:14 +0100

    package.json: Use `jest` instead of `mocha`

--- lint-test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let glob = require('glob').sync;
-
-let paths = glob('test/**/*.js').filter(path => !(/fixtures/).test(path));
-
-paths = paths.concat(glob('lib/**/*.js'));
-
-require('mocha-eslint')(paths);
